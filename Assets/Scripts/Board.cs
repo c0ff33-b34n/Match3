0a570c8b9c5eca81cdfa044f41342afda9ae8c20
Stuart Bowes <stuartbowes2000@gmail.com>
2020-05-22 11:31:10 +0100

    Initial project setup, add basic art assets and setup background game board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        Setup();
+    }
+    
+    private void Setup()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            }
+        }
+    }
+
+}
