333afa947d9c8d6a7557ffc06e35c4f341f18b05
Stuart Bowes <stuartbowes2000@gmail.com>
2020-05-22 14:35:47 +0100

    Move dot creation to Board script, Add Dot script with calculation of swipe angle when dot touched and released

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,12 +5,15 @@ public class Board : MonoBehaviour
     public int width;
     public int height;
     public GameObject tilePrefab;
+    public GameObject[] dots;
     private BackgroundTile[,] allTiles;
+    public GameObject[,] allDots;
 
     // Start is called before the first frame update
     void Start()
     {
         allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
         Setup();
     }
     
@@ -21,9 +24,15 @@ public class Board : MonoBehaviour
             for (int j = 0; j < height; j++)
             {
                 Vector2 tempPosition = new Vector2(i, j);
-                GameObject backgroundTile =  Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform; // Parent this backgroundTile GameObject to the Board object (to make it tidy in Hierarchy window)
                 backgroundTile.name = "( " + i + ", " + j + " )"; // Name the tiles with their grid position.
+
+                int dotToUse = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + ", " + j + " )"; // name dot tile with initial grid position.
+                allDots[i, j] = dot;
             }
         }
     }
---
+++ Assets/Scripts/Dot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour
+{
+
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    public float swipeAngle = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        Debug.Log("Swipe Angle:" + swipeAngle);
+    }
+}
