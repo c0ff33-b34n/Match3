50342e242c5c8fd9b090c1ee69342604ad9e06d6
Stuart Bowes <stuartbowes2000@gmail.com>
2020-06-01 16:52:53 +0100

    Fix color bombs so that they don't recall the color of the original tile before converted and blow up multiple color types

--- Assets/Scripts/Dot.cs
+++ Assets/Scripts/Dot.cs
@@ -71,6 +71,7 @@ public class Dot : MonoBehaviour
         isColorBomb = true;
         GameObject color = Instantiate(colorBomb, transform.position, Quaternion.identity);
         color.transform.parent = this.transform;
+        this.gameObject.tag = "Color";
     }
 
     public void MakeAdjacentBomb()
